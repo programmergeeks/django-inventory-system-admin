c40265e71871cba430e5ac1ff846e0a044246b1e
[James Alwin Aljecera] <[james.aljecera@awsys-i.com]>
2024-05-07 10:22:03 +0800

    Added 2 new html pages for nav link access and etc. (05/07 - 10:21)

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,12 +1,20 @@
-from django.http import HttpResponse
+# from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
 
-#Request for viewing index page via HttpResponse
+#Request for viewing index page via render
 def index(request):
-    return render(request,'index.html')
+    return render(request,'dashboard/index.html')
 
-#Request for viewing staff page via HttpResponse
+#Request for viewing staff page via render
 def staff(request):
-    return render(request,'staff.html')
+    return render(request,'dashboard/staff.html')
+
+#Request for viewing staff page via render
+def product(request):
+    return render(request,'dashboard/product.html')
+
+#Request for viewing staff page via render
+def order(request):
+    return render(request,'dashboard/order.html')
