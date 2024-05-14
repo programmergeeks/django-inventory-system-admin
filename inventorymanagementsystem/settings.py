fb2c58fdf3910fd734a2c1cbc35cc6b3caae40fc
[James Alwin Aljecera] <[james.aljecera@awsys-i.com]>
2024-05-14 16:00:00 +0800

    Adjusted settings for collectstatic files to get initiated (05/14 - 15:59)

--- inventorymanagementsystem/settings.py
+++ inventorymanagementsystem/settings.py
@@ -117,7 +117,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
-STATICFILES_DIRS = [BASE_DIR / "static"]  
+
+STATIC_ROOT = (BASE_DIR /"asert/")
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static"
+]  
 
 
 # Default primary key field type
