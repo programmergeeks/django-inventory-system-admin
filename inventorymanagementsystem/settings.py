8a85c68c431d49a5a1c242ab5053d4e5e54cd190
programmergeeks <aljecera.jamesalwin@gmail.com>
2024-07-04 22:08:58 +0800

    Password reset is completed and ready for project initial proposal (07/04 - 22:08)

--- inventorymanagementsystem/settings.py
+++ inventorymanagementsystem/settings.py
@@ -136,14 +136,17 @@ MEDIA_URL = "/media/"
 LOGIN_REDIRECT_URL = 'dashboard-index'
 
 LOGIN_URL = 'user-login'
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
-
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'sjsggcwmshskssz+conners@gmail.com'
-EMAIL_HOST_PASSWORD = 'inf0server'
+EMAIL_HOST_USER = 'aljecera.jamesalwin@gmail.com'
+EMAIL_HOST_PASSWORD = 'xtvv vmjb lqxd qvzw'
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
